31a899711473a77211a7261214b9d3b863d829e5
Bruno Junqueira <brunootaviojunqueira@gmail.com>
2015-08-04 17:26:38 -0300

    Launching login page by default
    Adding flags to remove the MainActivity of the historical
    user navigation. So when the user is on the LoginActivity,
    and press the return key, he will not access the MainActivity.

--- app/src/main/java/com/brunojunqueira/findfut/MainActivity.java
+++ app/src/main/java/com/brunojunqueira/findfut/MainActivity.java
@@ -2,6 +2,7 @@ package com.brunojunqueira.findfut;
 
 import java.util.Locale;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -39,6 +40,11 @@ public class MainActivity extends AppCompatActivity implements ActionBar.TabList
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
 
         // Set up the action bar.
         final ActionBar actionBar = getSupportActionBar();
